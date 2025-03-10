ed3cd0e17b13141c9633df8a9de3aa6e9dd6344b
SantiagoBecerraC <69489738+SantiagoBecerraC@users.noreply.github.com>
2025-03-09 21:16:14 -0500

    Update LinkedIn data extraction scripts

---
+++ linkedin_raw_data/test_get_profles.py
@@ -0,0 +1,7 @@
+
+from linkedin_api import Linkedin
+
+api = Linkedin('hjy.alder@outlook.com', 'Abced12sg!', refresh_cookies=True)
+urn = 'ACoAAD2zAhsB9-kge1ICtZ8u84N4wtVsxY7PD1o'
+profile = api.get_profile(urn_id=urn)
+print(profile)
